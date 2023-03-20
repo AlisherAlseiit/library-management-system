b2265f828badd9d4cc9761d44cacf2dec9130fd1
Alish <magawin51@gmail.com>
2023-03-21 00:07:04 +0600

    add middleware

--- app/main.py
+++ app/main.py
@@ -2,10 +2,22 @@ from fastapi import FastAPI
 from .routers import book, user, auth
 from . import models
 from .database import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 # models.Base.metadata.create_all(bind=engine)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(book.router)
 app.include_router(user.router)
